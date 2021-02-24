b36e3c2625f627077f4d6e3a53f764f75123474c
obinar <obinar@yandex.ru>
2021-02-25 01:24:20 +0300

    Начальный возврат модуля eded

---
+++ eded/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eded
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
